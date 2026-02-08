682b81818e3bb33ff6933c92e283e07bb3369ac5
eitelkeit0708 <liouyuze0708@gmail.com>
2026-02-08 21:41:58 +0800

    fix: update_config需主动调用init_plugin刷新状态，解决配置不生效问题 v1.4.5

--- plugins.v2/hanhanrescueseedingplus/__init__.py
+++ plugins.v2/hanhanrescueseedingplus/__init__.py
@@ -31,7 +31,7 @@ class HanHanRescueSeedingPlus(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/wikrin/MoviePilot-Plugins/main/icons/alter_1.png"
     # 插件版本
-    plugin_version = "1.4.4"
+    plugin_version = "1.4.5"
     # 插件作者
     plugin_author = "Eitelkeit"
     # 作者主页
@@ -243,6 +243,9 @@ class HanHanRescueSeedingPlus(_PluginBase):
                 new_key = key.replace("hanhanrescueseeding_", "")
                 new_config[new_key] = value
         
+        # 更新实时状态
+        self.init_plugin(new_config)
+
         # 调用父类方法更新配置
         return super().update_config(new_config)
 
